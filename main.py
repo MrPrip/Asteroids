95e9e52cbc1f91d25b54a35c8aa368f505bb17dc
Oliver Prip <ohag@itu.dk>
2025-04-01 00:19:47 +0200

    Finished collision between asteroids and shoots

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def main():
             for shot in shots:
                 if shot.collision(asteroid):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
